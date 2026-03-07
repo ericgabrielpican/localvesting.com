8d60219fffac6d40980f49c51db9fd08ecdc9c49
Eric Pican <ericgabrielpican@gmail.com>
2026-03-07 15:46:19 +0200

    fixed babel conflict

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-        require.resolve("expo-router/babel"),
-        "@react-native-community/datetimepicker"
-    ]
+    plugins: [require.resolve("expo-router/babel")],
   };
 };
